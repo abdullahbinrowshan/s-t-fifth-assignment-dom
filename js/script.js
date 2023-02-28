488cc8311b618aae13f2124cd606346a15588332
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-02-28 21:12:00 +0600

    selected player names added to the dom

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function selectedPlayer(PlayerName, btnId) {
+    const playerOlList = document.getElementById('player-list');
+    const newPlayerLi = document.createElement('li');
+    newPlayerLi.innerHTML = `<span class="text-base font-semibold leading-tight text-gray-500 dark:text-gray-400">${PlayerName}</span>`
+    playerOlList.appendChild(newPlayerLi);
     newPlayerLi.classList.add('text-lg', 'font-bold', 'leading-tight', 'text-gray-500', 'dark:text-gray-600');
     const selectedBtn = document.getElementById(btnId);
     selectedBtn.classList.add('cursor-not-allowed', 'bg-slate-500');
